5c2a232d5ecb812d32bff8c6faf7fc715a9aff1c
Kamylle Wilgozz <kamyllewilgozzd@gmail.com>
2023-05-01 13:59:47 -0300

    inicio tela de login

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'semantic-ui-css/semantic.min.css';
+import './index.css';
 import Login from './pages/login';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,6 +1,6 @@
 import { useState, useContext } from 'react';
 import './index.css';
-import { Button, Input } from 'semantic-ui-react'
+import { Button, Grid, Input, Image, Container, GridRow, Form, FormField } from 'semantic-ui-react'
 
 function Login() {
   const [email, setEmail] = useState();
@@ -11,15 +11,45 @@ function Login() {
 
   return (
     <div>
-        <div className='card'>
-            <h3>Login</h3>
-            <p>Bem-vindo novamente! Preencha os campos abaixo  para acessar sua conta</p>
-            <Input placeholder='Digite seu e-mail' />
-            <Input placeholder='Senha' />
-            <p>Não conseguiu acessar sua conta?</p>
-            <Button>Entrar</Button>
-            <p>Ainda não possui uma conta? Clique aqui</p>
-        </div>
+      <Container fluid>
+        <Grid>
+          <Grid.Row>
+
+            <Grid.Column computer={4} only='computer'>
+              <Image src='https://react.semantic-ui.com/images/wireframe/paragraph.png' />
+            </Grid.Column>
+
+
+            <Grid.Column tablet={16} computer={8}>
+              <div className='card'>
+                <h3>Login</h3>
+                <p className='subtituloLogin'>Bem-vindo novamente! Preencha os campos abaixo  para acessar sua conta</p>
+                <Form>
+
+                  <Form.Field required>
+                    <Input placeholder='Digite seu e-mail' />
+                  </Form.Field>
+
+                  <Form.Field required>
+                    <Input placeholder='Senha' />
+                  </Form.Field>
+
+                  <p className='naoConseguiuAcessarSuaConta'>Não conseguiu acessar sua conta?</p>
+                  <Button color='mediumblue' type='submit'>Entrar</Button>
+                </Form>
+
+                <p className='aindaNaoPossuiUmaConta'>Ainda não possui uma conta? Clique aqui</p>
+              </div>
+            </Grid.Column>
+
+
+            <Grid.Column computer={4} only='computer'  >
+              <Image src='https://react.semantic-ui.com/images/wireframe/paragraph.png' />
+            </Grid.Column>
+
+          </Grid.Row>
+        </Grid>
+      </Container >
     </div>
   );
 }
